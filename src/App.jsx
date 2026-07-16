76be033773165e6aa50cb8cf7a5fe9a2544ac00f
Ubuntu <ubuntu@ip-172-31-24-63.ec2.internal>
2026-07-16 12:42:04 +0000

    Deploy React app with Nginx on Ubuntu EC2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function App() {
+  return (
+    <div>
+      <h1>Favour React App</h1>
+      <h2>DevOps Micro Internship Assignment 2</h2>
+      <p>Deployed on Ubuntu EC2 using Nginx</p>
+      <p>Date: July 16, 2026</p>
+    </div>
+  );
+}
+
+export default App;
